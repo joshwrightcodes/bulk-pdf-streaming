0d58cb073580a697c842c0064a03173b026daca9
Josh Wright <josh.wright@edapp.com>
2022-03-11 12:30:12 +1100

    Set up S3 streaming and localstack. Updated doco

--- src/PdfStreaming/Program.cs
+++ src/PdfStreaming/Program.cs
@@ -1,12 +1,20 @@
 using System.IO.Compression;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+using LocalStack.Client.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddLocalStack(builder.Configuration)
+    .AddDefaultAWSOptions(builder.Configuration.GetAWSOptions())
+    .AddAwsService<IAmazonS3>(); // !!!!!! different to normal .AddAWSService !!!!!!
 
 var app = builder.Build();
 
@@ -41,28 +49,39 @@ app.MapGet("/static-pdf", async (
     }
 });
 
-app.MapGet("/static-s3", async (
+app.MapGet("/static-s3", async(
     HttpContext context,
-    int? generate,
+    int ? generate,
+    [FromServices]IAmazonS3 s3Client,
     CancellationToken cancellationToken) =>
 {
-    var staticPdfPath = Path.Combine(app.Environment.ContentRootPath, "Documents", "SamplePDF.pdf");
+    const string bucket = "demo";
+    const string fileKey = "a/folder/SamplePDF.pdf";
 
     var docCount = generate ?? 1;
 
     context.Response.ContentType = "application/octet-stream";
     context.Response.Headers.Add("Content-Disposition", "attachment; filename=\"document.zip\"");
 
+    var util = new TransferUtility(s3Client);
+    
     using var archive = new ZipArchive(context.Response.BodyWriter.AsStream(), ZipArchiveMode.Create);
 
     for (var i = 1; i <= docCount; i++)
     {
-        var filenameWithoutExtension = Path.GetFileNameWithoutExtension(staticPdfPath);
-        var filename = $"{filenameWithoutExtension}_{i:0000000000}.pdf";
-        var entry = archive.CreateEntry(filename);
-        await using var entryStream = entry.Open();
-        await using var fileStream = File.OpenRead(staticPdfPath);
-        await fileStream.CopyToAsync(entryStream, cancellationToken);
+        try
+        {
+            var filenameWithoutExtension = Path.GetFileNameWithoutExtension(fileKey);
+            var filename = $"{filenameWithoutExtension}_{i:0000000000}.pdf";
+            var entry = archive.CreateEntry(filename);
+            await using var entryStream = entry.Open();
+            await using var fileStream = await util.OpenStreamAsync(bucket, fileKey, cancellationToken);
+            await fileStream.CopyToAsync(entryStream, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
     }
 });
 
